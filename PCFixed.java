ccd3d9035ece291da946048a5f5e0063e89dc45f
RamyaPG <53213236+RamyaPG@users.noreply.github.com>
2019-09-24 12:14:05 +0530

    Add files via upload

---
+++ PCFixed.java
@@ -0,0 +1,80 @@
+package Applicationthread;
+class Q
+{
+    int n;
+    boolean valueset=false;
+    synchronized int get()
+    {
+        while(!valueset)
+            try
+            {
+                wait();
+            }catch(InterruptedException e)
+            {
+                System.out.println("InterruptedException caught");
+            }
+        System.out.println("Consumer:"+n);
+        valueset=false;
+        notify();
+        return n;
+     }
+     synchronized void put(int n)
+     {
+        while(valueset)
+            try
+            {
+                wait();
+            }catch(InterruptedException e)
+            {
+                 System.out.println("InterruptedException caught");
+                    
+            }
+        this.n=n;
+        valueset=true;
+        System.out.println("Producer:"+n);
+        notify();
+        }
+ }
+ class Producer implements Runnable
+ {
+    Q q;
+    Producer(Q q)
+    {
+        this.q=q;
+        new Thread(this,"Producer").start();
+    }
+    public void run()
+    {
+        int i=0;
+        while(true)
+        {
+            q.put(i++);
+        }
+    }
+ }
+ class Consumer implements Runnable
+ {
+    Q q;
+    Consumer(Q q)
+    {
+        this.q=q;
+         new Thread(this,"Consumer").start();
+    }
+    public void run()
+    {
+        while(true)
+        {
+        q.get();
+        }
+    }
+ }
+public class PCFixed 
+{
+    public static void main(String args[])
+    {
+        Q q=new Q();
+        new Producer(q);
+        new Consumer(q);
+        System.out.println("Press Control-c to stop.");
+    }
+}
